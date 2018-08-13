0340be1502c03a3895402ec146991444e49cb3bc
Alex Navarro <lexn29@gmail.com>
2018-08-12 19:55:22 -0600

    Added new components and import modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from "@angular/common/http";
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxPaginationModule } from 'ngx-pagination';
+
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { AlbumsComponent } from './albums/albums.component';
+import { FooterComponent } from './footer/footer.component';
+import { AlbumFilterPipe } from './albums/album-filter.pipe';
+import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    AlbumsComponent,
+    AlbumFilterPipe,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    NgxPaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
