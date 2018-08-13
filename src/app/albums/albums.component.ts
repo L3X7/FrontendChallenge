853e9cc70a25c6fbfd2d5b8eaa02f8e0e29e0d77
Alex Navarro <lexn29@gmail.com>
2018-08-12 20:01:31 -0600

    Created album component

---
+++ src/app/albums/albums.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ItunesApiService } from "../services/itunes-api.service"
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-albums',
+  templateUrl: './albums.component.html',
+  styleUrls: ['./albums.component.less']
+})
+export class AlbumsComponent implements OnInit {
+
+  private topAlbums: any;
+  private apiData: any;
+  private p: 1;
+  private albumSelected: any;
+  public searchTerm: string;
+  
+  constructor(
+    private iTunesApiService: ItunesApiService,
+    private modalService: NgbModal
+  ) { }
+
+  ngOnInit() {
+    this.getTopAlbums();
+  }
+
+  public getTopAlbums() {
+    this.iTunesApiService.getTopAlbums().subscribe((data: Array<object>) => {
+      
+      this.apiData = data;
+      this.topAlbums = this.apiData.feed.entry;
+
+    })
+  }
+
+  open(content, data) {
+    this.albumSelected = data;
+    this.modalService.open(content, { windowClass: 'dark-modal' });
+  }
+  
+}
